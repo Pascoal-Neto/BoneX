d605b4395167b20c6ab1548d32bd1d82f50ea490
Pascoal-Neto <56798710+Pascoal-Neto@users.noreply.github.com>
2021-10-24 00:22:50 -0300

    Primeira versão do arquivo
    Primeira versão enviada para o versionamento do arquivo com o código fonte do arquivo bonex

---
+++ projeto_bonex.ino
@@ -0,0 +1,22 @@
+int ledPin=05;
+int sensorPin=A0;
+boolean val =10;
+
+void setup(){
+  pinMode(ledPin, OUTPUT);
+  pinMode(sensorPin, INPUT);
+  Serial.begin (9600);
+}
+  
+void loop (){
+  val =digitalRead(sensorPin);
+  //Serial.println (val);
+  // when the sensor detects a signal above the threshold value, LED flashes
+  if (val==HIGH) {
+    digitalWrite(ledPin, HIGH);
+    
+  }
+  else {
+    digitalWrite(ledPin, LOW);
+  }
+}
